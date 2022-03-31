b6f2aba8a3c28cb0b5e9b1a2d1a2fa6cb36ce11a
Larissa Brencher <larissa.brencher@gmail.com>
2022-03-31 18:37:41 +0200

    Add setup files and versioneer

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import setuptools
+import versioneer
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setup(
+    name="slnn",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Larissa Brencher",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://github.com/LarissaBrencher/SLNN/",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+    install_requires=["numpy", "matplotlib", "torch", "sympy", "pylab", "json", "random", "datetime", "time", "inspect", "os"],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
