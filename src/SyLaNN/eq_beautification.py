14cb8724f7fd7a37c4b8337cf7af065c8655f240
Larissa Brencher <larissa.brencher@gmail.com>
2022-09-01 13:22:41 +0200

    Finish raw version of equation beautification
    TODO test outputs, improve code comments

--- src/SyLaNN/eq_beautification.py
+++ src/SyLaNN/eq_beautification.py
@@ -1,6 +1,6 @@
-from tkinter import FALSE
 from sympy import *
 import json
+import copy
 
 def extract_sympyEq(lambda_str):
     split_here = lambda_str.find(':') + 2 # cut off colon and space
@@ -30,7 +30,7 @@ def createResultsDict(checkEN, path_name, file_format, gamma_list=[]):
 
     if checkEN is True:
         # fill results dictionary with simulation results (nested dictionary of dicionaries)
-        for gammaIdx in gamma_list:
+        for gammaIdx, gammaVal in enumerate(gamma_list):
             current_key = gammaIdx
             current_json_name = path_name + str(current_key) + file_format
             # read current results file and save into dictionary
@@ -56,7 +56,7 @@ def createBeautifDict(checkEN, results_dict, gamma_list=[]):
 
     if checkEN is True:
         # loop over found equations
-        for gammaIdx in gamma_list:
+        for gammaIdx, gammaVal in enumerate(gamma_list):
             tmp_eqDict = {}
             current_key = gammaIdx
             currentEq_str = results_dict[gammaIdx]["found_eq"]
@@ -125,8 +125,49 @@ def createBeautifDict(checkEN, results_dict, gamma_list=[]):
 
     return eqs_beautified
 
-def eq_beautifPrint(eqs_dict):
-    pass
+def removeDuplicates_dictValues(eqs_dict):
+    result_dict = {}
+    # loop over values and do not include already existing values in new dict
+    for key, value in eqs_dict.items():
+        if value not in result_dict.values():
+            result_dict[key] = value
+    # return dict with removed duplicate equations
+    return result_dict
 
-def saveEqBeautifDict(eqs_dict):
-    pass
+def eq_beautifPrint(checkEN, eqs_dict, gamma_list=[]):
+    # TODO still ugly with long expressions...
+    if checkEN is True:
+        for gammaIdx, gammaVal in enumerate(gamma_list):
+            print('Predicted equation: ')
+            print('(trained with elastic net penalty at L1-ratio %f)' % (gammaVal))
+            print(eqs_dict[gammaIdx]['long_expanded'])
+            alternate_forms = removeDuplicates_dictValues(eqs_dict[gammaIdx])
+            alternate_forms.pop('long_expanded') # remove initial expression
+            print('Alternate forms: ')
+            for value in alternate_forms.values():
+                print(value)
+            # for better readability after each gamma iteration (bottom line)
+            print('#--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--#')
+    if checkEN is False:
+        print('Predicted equation: ')
+        print(eqs_dict['long_expanded'])
+        alternate_forms = removeDuplicates_dictValues(eqs_dict)
+        alternate_forms.pop('long_expanded') # remove initial expression
+        print('Alternate forms: ')
+        for value in alternate_forms.values():
+            print(value)
+
+def sympy2str(sympyFct, vars_list):
+    var_sym = symbols(vars_list)
+    lmb_fct_str = utilities.lambdify.lambdastr(var_sym, sympyFct)
+    return lmb_fct_str
+
+def saveEqBeautifDict(eqs_dict, vars_list, save_file_name):
+    eqs_dict_copy = copy.deepcopy(eqs_dict)
+    for key, value in eqs_dict.items():
+        value_str = sympy2str(value, vars_list)
+        eqs_dict_copy.update({key : value_str})
+
+    # save dictionary to json file in corresponding simulation folder
+    with open(save_file_name, 'w') as outfile:
+        json.dump(eqs_dict_copy, outfile, indent = 4)
--- src/SyLaNN/run_eq-postprocessing.py
+++ src/SyLaNN/run_eq-postprocessing.py
@@ -2,6 +2,7 @@
 import os
 import eq_beautification as eqBeauty
 from sympy import *
+import numpy as np
 
 ######## User inputs: folder name, file name, EN gamma list ########
 # folder name and file name (without gammaIdx numbering)
@@ -26,17 +27,24 @@ checkEN = True
 gamma_nSteps = 10
 gamma_start, gamma_stop = 0, 1+gamma_nSteps
 # as a list of integers and list of strings
-gamma_list = list(range(gamma_start, gamma_stop))
+gamma_list = [*np.arange(gamma_start, gamma_stop, gamma_nSteps)]
 
 ######## Beautify predicted equation ########
+# TODO needed? or just save and print within ifs
+# TODO comment everything better (especially in eq_beautificaton)
 results_dict = {}
 eqs_beautif = {}
 if checkEN is True:
     results_dict = eqBeauty.createResultsDict(checkEN, path_name, file_format, gamma_list)
     eqs_beautif = eqBeauty.createBeautifDict(checkEN, results_dict, gamma_list)
+    vars_str = results_dict[0]["variables_str"]
+    eqBeauty.eq_beautifPrint(checkEN, eqs_beautif, gamma_list)
+    save_file_name = path_name + '_beautified' + file_format
+    eqBeauty.saveEqBeautifDict(eqs_beautif, vars_str, save_file_name, gamma_list)
 if checkEN is False:
     results_dict = eqBeauty.createResultsDict(checkEN, path_name, file_format)
     eqs_beautif = eqBeauty.createBeautifDict(checkEN, results_dict)
-
-# TODO save several options on simplified expressions and print them in a WolframAlpha manner
-# TODO include gamma specification into printing out the alternative forms
+    vars_str = results_dict["variables_str"]
+    eqBeauty.eq_beautifPrint(checkEN, eqs_beautif)
+    save_file_name = path_name + '_beautified' + file_format
+    eqBeauty.saveEqBeautifDict(eqs_beautif, vars_str, save_file_name)
